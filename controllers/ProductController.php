8793a21547cc5b95a4f9e7b1d4d936c4e92bdc3f
trongio <giokakabadze50@gmail.com>
2021-03-19 01:50:59 +0400

    Update ProductController.php

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -8,7 +8,6 @@ require_once __DIR__ . '/../db/Database.php';
 $db=new Database();
 
 if(isset($_POST['delete']) && isset($_POST['checkbox'])){
-    var_dump($_POST);
     $ckboxes=$_POST['checkbox'];
     foreach ($ckboxes as $box){
         $db->Delete($box);
