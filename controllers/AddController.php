743537641977a1451e3429029a928bb4e7586338
trongio <giokakabadze50@gmail.com>
2021-03-28 19:18:50 +0400

    added negative checker

--- controllers/AddController.php
+++ controllers/AddController.php
@@ -39,6 +39,9 @@ if (!$data['width']) {
 if (!$data['length']) {
     $errors['length'] = "length is required";
 }
+if (!$data['price']) {
+    $errors['price'] = "price is required";
+}
 if (!$data['sku']) {
     $errors['sku'] = "sku is required";
 }
@@ -48,20 +51,22 @@ if (!$data['type']) {
 if (!$data['name']) {
     $errors['name'] = "name is required";
 }
-if (!$data['price']) {
-    $errors['price'] = "price is required";
-}
 if ($uniqueSku) {
     $errors['sku'] = "sku already in use";
 }
-
+if (intval($data['size'],$base = 10)<0) $errors['size'] = "size must be more than 0";
+if (intval($data['weight'],$base = 10)<0) $errors['weight'] = "weight must be more than 0";
+if (intval($data['height'],$base = 10)<0) $errors['height'] = "height must be more than 0";
+if (intval($data['width'],$base = 10)<0)$errors['width'] = "width must be more than 0";
+if (intval($data['length'],$base = 10)<0) $errors['length'] = "length must be more than 0";
+if (intval($data['price'],$base = 10)<0) $errors['price'] = "price must be more than 0";
 
 if (empty($errors)) {
     $instance = new $data['type']($data['sku'], $data['type'], $data['name'], $data['price'], $data['data']);
     $instance->add();
     echo "success";
 }
-else {
+ {
     foreach ($errors as $error) {
         echo "<p>";
         echo $error;
